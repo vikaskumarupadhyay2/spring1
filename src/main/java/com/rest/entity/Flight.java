4564c18e13d3cc08601a65db07a597684353229c
vikas <vikaskumarupadhyay90@gmail.com>
2022-08-13 13:23:40 +0530

    commit

--- src/main/java/com/rest/entity/Flight.java
+++ src/main/java/com/rest/entity/Flight.java
@@ -2,6 +2,9 @@ package com.rest.entity;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -10,6 +13,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Flight {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String flightNumber;
 		
 	@JsonManagedReference
